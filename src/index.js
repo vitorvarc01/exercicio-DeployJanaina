07e96925ec2f6f12181c67cd687dba5886b48fd3
janainabatista <janainadutrabatista@gmail.com>
2023-12-19 20:40:40 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+const express =require('express');
+const cors = require('cors');
+const knex =require('./conexao')
+
+const app = express()
+
+app.use (express.json())
+app.use(cors())
+app.get('/',async(req, res)=>{
+    try{
+    const nomes = await knex('nomes')
+    res.status(202).json(nomes)
+        
+    }catch(error){
+    console.log(error)
+    res.status(500).json("erro no servidor")
+}
+})
+
+const PORT =process.env.PORT ||3000
+
+app.listen(PORT)
+
+
